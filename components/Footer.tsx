3b61ec89c2e3f280248152ae8587348ed5bb426f
patrickNwafo <patrickchinwafor@gmail.com>
2023-10-27 16:42:02 +0100

    starting files

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+    return <div>Footer2</div>;
+};
+
+export default Footer;
---
+++ components/Navbar.tsx
@@ -0,0 +1,5 @@
+const Navbar = () => {
+    return <nav className="border-2 border-red-500">Navbar</nav>;
+};
+
+export default Navbar;
